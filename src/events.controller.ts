1a4c6d3b5e5340e37ba52ea8ffb2de2546b016b0
“Santu <roysantu2002@yahoo.com>
2022-04-28 11:10:03 +0530

    first commit

---
+++ src/events.controller.ts
@@ -0,0 +1,41 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { CreateEventDto } from './create-event.dto';
+
+@Controller('/events')
+export class EventsController {
+  @Get()
+  findAll() {
+    return [
+      { id: 1, name: 'First event' },
+      { id: 2, name: 'Second event' },
+    ];
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id) {
+    return { id: 1, name: 'First event' };
+  }
+
+  @Post()
+  create(@Body() input: CreateEventDto) {
+    return input;
+  }
+
+  @Patch(':id')
+  update(@Param('id') id, @Body() input) {
+    return input;
+  }
+
+  @Delete(':id')
+  @HttpCode(204)
+  remove(@Param('id') id) {}
+}
